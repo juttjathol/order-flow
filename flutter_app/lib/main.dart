441689fa6a4b083d858d37b7f601b1fd1969828e
juttjathol <aa1345471@gmail.com>
2026-08-12 12:27:17 +0800

    Runtime: main.dart init + LocalServer /api/event

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
+import 'core/state/app_controller.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: OrderFlowApp()));
+  final container = ProviderContainer();
+  await container.read(appControllerProvider).init();
+  runApp(UncontrolledProviderScope(
+    container: container,
+    child: const OrderFlowApp(),
+  ));
 }
